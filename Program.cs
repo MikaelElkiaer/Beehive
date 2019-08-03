ed382c2e0fde8d13255da16996f2c26744648a2f
MikaelElkiaer <mikaelec+github@gmail.com>
2019-08-03 16:23:52 +0200

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace beehive
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            DockerClient client = new DockerClientConfiguration(new Uri("unix:///var/run/docker.sock"))
+                .CreateClient();
+            IList<ContainerListResponse> containers = await client.Containers.ListContainersAsync(new ContainersListParameters
+                    {
+                        All = true 
+                    });
+
+                foreach (var c in containers)
+                            Console.WriteLine(JsonConvert.SerializeObject(c));
+        }
+    }
+}
